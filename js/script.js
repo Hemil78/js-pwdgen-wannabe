db1a487f1fe04872a77105947c990258c0b9db4b
Hemil78 <zanattahemil@gmail.com>
2021-07-28 16:11:29 +0200

    stile

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 //inserisci il tuo nome
-var name = prompt('Inserisci il tuo nome')
+var name = prompt('Inserisci il tuo nome');
+
 
 
 //inserisci il tuo cognome
-var lastname = prompt('Inserisci il tuo cognome')
+var lastname = prompt('Inserisci il tuo cognome');
+
 
 
 //qual'è il tuo colore preferito
-var color = prompt('Il tuo colore preferito')
+var color = prompt('Il tuo colore preferito');
+
 
 
 //concatenare tutti i valori in una password
 var password = name + lastname + color + 40;
 
 
+
 //password nella console
-console.log(password)
+console.log(password);
 
 
 //inserisco la password all'interno dell tag h1
